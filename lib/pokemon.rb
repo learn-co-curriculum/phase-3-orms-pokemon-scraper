a0249dea581622639090c47377946a89d995b141
Danny-Sullivan <dsully360@gmail.com>
2015-11-16 17:42:25 -0500

    removed solution

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,20 +1,2 @@
 class Pokemon
-  attr_accessor :id, :name, :type, :db
-
-  def initialize(id, name, type, db)
-    @id = id
-    @name = name
-    @type = type
-    @db = db
-  end
-
-  def self.save(name, type, db)
-    db.execute("INSERT INTO pokemon (name, type) VALUES (?, ?)", name, type)
-  end
-
-  def self.find(id_num, db)
-    pokemon_info = db.execute("SELECT * FROM pokemon WHERE id=?", id_num).first
-  end
-
-
 end
