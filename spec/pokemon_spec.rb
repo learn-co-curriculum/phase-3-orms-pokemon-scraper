2520ef69d62222845425944d2c6e0c8c5d7058f2
Charles Pletcher <cpletcher2@gmail.com>
2016-06-27 12:44:57 -0400

    Add bonus specs (marked as pending)

--- spec/pokemon_spec.rb
+++ spec/pokemon_spec.rb
@@ -21,7 +21,7 @@ describe "Pokemon" do
 
   describe ".save" do
     it 'saves an instance of a pokemon with the correct id' do
-      new_pokemon = Pokemon.save("Pikachu", "fire", @db)
+      Pokemon.save("Pikachu", "fire", @db)
 
       pikachu_from_db = @db.execute("SELECT * FROM pokemon WHERE name = 'Pikachu'")
       expect(pikachu_from_db).to eq([[1, "Pikachu", "fire"]])
@@ -30,12 +30,42 @@ describe "Pokemon" do
 
   describe ".find" do
     it 'finds a pokemon from the database' do
-      newer_pokemon = Pokemon.save("Pikachu", "fire", @db)
+      Pokemon.save("Pikachu", "fire", @db)
 
       pikachu_from_db = Pokemon.find(1, @db)
       expect(pikachu_from_db).to eq([[1, "Pikachu", "fire"]])
     end
   end
 
+  describe "BONUS" do
+    # The find method creates a new Pokemon after selecting their row from the database by their id number.
+    let(:pikachu){Pokemon.find(25, @db)}
+    let(:magikarp){Pokemon.find(129, @db)}
 
+    before do
+      @sql_runner.execute_create_hp_column
+    end
+
+    it "knows that a pokemon have a default hp of 60" do
+      pending "Implement the bonus section of the README. Then remove this line."
+      expect(@db.execute("knows_default_hp").flatten.first).to eq(60)
+    end
+
+    # So Ian and you have decided to battle.  He chose Magikarp (rookie mistake), and you chose Pikachu.
+    # He used splash. It wasn't very effective. It did one damage.
+    it "alters Pikachu's hp to 59" do
+      pending "Implement the bonus section of the README. Then remove this line."
+
+      pikachu.alter_hp(59)
+      expect(@db.execute("alter_hp").flatten.first).to eq(59)
+    end
+
+    # Now we alter Magikarp's hp
+    it "alters Magikarp's hp" do
+      pending "Implement the bonus section of the README. Then remove this line."
+
+      magikarp.alter_hp(0)
+      expect(@db.execute("alter_hp").flatten.first).to eq(0)
+    end
+  end
 end
