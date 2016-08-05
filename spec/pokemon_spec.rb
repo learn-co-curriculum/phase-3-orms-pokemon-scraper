f4b9e5ee8cf41c893cbccc168a1090ad5a3c6509
JJ Seymour <jjseymour@gmail.com>
2016-08-05 13:34:26 -0400

    fixed syntax error (missing 'end')

--- spec/pokemon_spec.rb
+++ spec/pokemon_spec.rb
@@ -17,6 +17,7 @@ describe "Pokemon" do
       expect(pokemon).to respond_to(:type)
       expect(pokemon).to respond_to(:db)
     end
+  end
 
   describe ".save" do
     it 'saves an instance of a pokemon with the correct id' do
