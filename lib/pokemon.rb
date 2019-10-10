174e134b503929e7f23e714b6658cf6e2f3d5ff7
github username <github email address>
2019-10-10 10:54:34 -0700

    removed method

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,5 +1,5 @@
 class Pokemon
-  attr_accessor :id, :name, :type, :hp, :db
+  attr_accessor :id, :name, :type, :db
 
   def self.save(name, type, db)
     db.execute("INSERT INTO pokemon (name, type) VALUES (?, ?)", name, type)
@@ -7,14 +7,11 @@ class Pokemon
 
   def self.find(id_num, db)
     pokemon_info = db.execute("SELECT * FROM pokemon WHERE id=?", id_num).flatten
-    Pokemon.new(id: pokemon_info[0], name: pokemon_info[1], type: pokemon_info[2], hp: pokemon_info[3], db: db)
+    Pokemon.new(id: pokemon_info[0], name: pokemon_info[1], type: pokemon_info[2], db: db)
   end
 
-  def initialize(id:, name:, type:, hp: nil, db:)
-    @id, @name, @type, @hp, @db = id, name, type, hp, db
+  def initialize(id:, name:, type:, db:)
+    @id, @name, @type, @db = id, name, type, db
   end
 
-  def alter_hp(new_hp, db)
-    db.execute("UPDATE pokemon SET hp = ? WHERE id = ?", new_hp, self.id)
-  end
 end
